1ac880fb6d8271fe45451f95b0fd0e802eb2bd57
webothdo <joshuaogboloo@gmail.com>
2024-05-22 17:54:01 +0100

    finished message page

---
+++ pages/message.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const query = useRoute().query
+const textAreaRef = ref('')
+
+onMounted(() => {
+    console.log(textAreaRef.value)
+})
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center gap-5 w-fit mx-auto mt-20">
+        <div class="flex flex-col bg-white w-[260px] pb-3 rounded-md focus-visible:outline-none ">
+            <textarea v-model="textAreaRef" name="" id="" cols="30" rows="10" placeholder="Enter your message"
+                class="rounded-md py-5 px-4 w-[260px] h-[190px] resize-none focus:outline-none font-[Arimo] text-base text-[#1F000A]">What yu say</textarea>
+            <p class="self-end mr-3 font-[Arimo] text-[13px] text-[#474747]">{{ textAreaRef.length }} / 250</p>
+        </div>
+        <button class="bg-[#fd1c5c] text-white px-7 py-1 w-full font-[Arimo] text-[21px] rounded-md">
+            Send
+        </button>
+    </div>
+</template>
