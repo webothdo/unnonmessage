7218fbfb3c85afa7f5fcf9ff374c42d2e746d66c
webothdo <joshuaogboloo@gmail.com>
2024-05-24 15:52:08 +0100

    fixed logo path

--- components/NavbarComp.vue
+++ components/NavbarComp.vue
@@ -24,12 +24,12 @@ const copyLink = () => {
 
 <template>
     <nav v-if="auth.loggedIn" class="flex justify-between items-center">
-        <NuxtLink v-if="$route.path === '/'" to="/profile"><img :src="`./logo.png`" alt=""></NuxtLink>
+        <NuxtLink v-if="$route.path === '/'" to="/profile"><img src="../assets/Logo.png" alt=""></NuxtLink>
         <button v-else-if="$route.path === '/profile'" @click="copyLink"
             class="bg-[#757575] text-white px-5 py-1 w-fit font-[Arimo] text-[16px] rounded-md">
             copy link
         </button>
-        <NuxtLink v-else to="/"><img :src="`./logo.png`" alt=""></NuxtLink>
+        <NuxtLink v-else to="/"><img src="../assets/Logo.png" alt=""></NuxtLink>
         <NuxtLink to="/api/logout" external>
             <button class="bg-[#fd1c1c] text-white px-5 py-1 w-fit font-[Arimo] text-[16px] rounded-md">
                 Logout
@@ -37,7 +37,7 @@ const copyLink = () => {
         </NuxtLink>
     </nav>
     <nav v-else class="flex justify-between items-center">
-        <NuxtLink to="/"><img :src="`/logo.png`" alt=""></NuxtLink>
+        <NuxtLink to="/"><img src="../assets/Logo.png" alt=""></NuxtLink>
         <NuxtLink to="/api/login" external>
             <button class="bg-[hsl(0,0%,0%)] text-white px-5 py-1 w-fit font-[Arimo] text-[16px] rounded-md">
                 Login
